e56b6d51f8fb4be29f9f9324b5ee84451b10a2c4
kiet-git <89214459+kiet-git@users.noreply.github.com>
2022-06-24 23:23:17 +0700

    fix init

--- core/init.php
+++ core/init.php
@@ -4,11 +4,12 @@ ob_start();
 session_start();
 if(!isset($_SESSION['logged_in'])) {
     $_SESSION['logged_in'] = false;
+    $_SESSION['user_id'] = -1;
 }
 
 include_once "core/DB.php";
 DB::getInstance();
-DB::connect("localhost", "root", "", "2022PollAppDB"); //pw = "" for XAMP
+DB::connect("localhost", "root", "root", "2022PollAppDB"); //pw = "" for XAMP
 $conn = DB::getConn();
 // auto load classes
 spl_autoload_register(function($className) {
